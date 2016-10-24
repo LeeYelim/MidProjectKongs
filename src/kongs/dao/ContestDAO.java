1350915e0e606b1f9abf6d8d779075775e1ef0ce
joyoungyun <asacans@gmail.com>
2016-10-24 14:20:57 +0900

    contestDAO

--- src/kongs/dao/ContestDAO.java
+++ src/kongs/dao/ContestDAO.java
@@ -11,4 +11,26 @@ public interface ContestDAO {
 	 * Contest에 있는 전체레코드 조회
 	 */
 	List<Contest> selectAll() throws SQLException;
+	
+	
+	/**
+	 * Contest에 레코드 삽입
+	 * 공모전 정보 게시판의 게시글 등록
+	 */
+	int insert(Contest contest) throws SQLException;
+	
+	
+	/**
+	 * Contest에 레코드 수정
+	 * 공모전 정보 게시판의 게시글 수정
+	 */
+	int update(Contest contest) throws SQLException;
+	
+	
+	/**
+	 * Contest에 레코드 삭제
+	 * 공모전 정보 게시판의 게시글 삭제
+	 */
+	int delete(String contestNum) throws SQLException;
+	
 }
--- src/kongs/dao/ContestDAOimpl.java
+++ src/kongs/dao/ContestDAOimpl.java
@@ -30,8 +30,9 @@ public class ContestDAOimpl implements ContestDAO {
 			ps = con.prepareStatement("select * from contest");
 			rs =ps.executeQuery();
 			while(rs.next()) {
-				list.add(new Contest(rs.getInt(1), 
-						title, sponser, category, startDate, endDate, clickCount, regDate, photoName))
+				list.add(new Contest(rs.getInt("num"), rs.getString("title"), rs.getString("sponser"), 
+						rs.getString("category"), rs.getString("startDate"), rs.getString("endDate"), 
+						rs.getInt("clickCount"), rs.getString("regDate"), rs.getString("photoName")));
 			}
 			
 		} finally {
@@ -39,6 +40,95 @@ public class ContestDAOimpl implements ContestDAO {
 		}
 		
 		return list;
+	}
+
+	
+	/**
+	 * Contest에 레코드 삽입
+	 * 공모전 정보 등록
+	 */
+	@Override
+	public int insert(Contest contest) throws SQLException {
+		
+		Connection con = null;
+		PreparedStatement ps = null;
+		int result = 0;
+		
+		try {
+	
+			con = DBUtil.getConnection();
+			ps = con.prepareStatement("insert into contest(title,sponser,startday,endday,photoname,clickcount,category,regdate,contestnum) "
+					+ "values(?,?,?,?,?,?,?,sysdate,?)");
+			ps.setString(1, contest.getTitle());
+			ps.setString(2, contest.getSponser());
+			ps.setString(3, contest.getStartDate());
+			ps.setString(4, contest.getEndDate());
+			ps.setString(5, contest.getPhotoName());
+			ps.setInt(6, contest.getClickCount());
+			ps.setString(7, contest.getCategory());
+			//TODO 시퀀스넘버넣어주기
+			result = ps.executeUpdate();
+	
+		} finally {
+			DBUtil.dbClose(con, ps, null);
+		}
+		
+		return result;
+	}
+
+	
+	/**
+	 * Contest에 레코드 수정
+	 * 공모전 정보 수정
+	 */
+	@Override
+	public int update(Contest contest) throws SQLException {
+		Connection con = null;
+		PreparedStatement ps = null;
+		int result = 0;
+		
+		try{
+			
+			con = DBUtil.getConnection();
+			ps = con.prepareStatement("update contest set sponser=?,startday=?,endday=?,photonum=?, category=? where contestnum=?");
+			ps.setString(1, contest.getSponser());
+			ps.setString(2, contest.getStartDate());
+			ps.setString(3, contest.getEndDate());
+			ps.setString(4, contest.getPhotoName());
+			ps.setString(5, contest.getCategory());
+			ps.setInt(6, contest.getNum());
+			result = ps.executeUpdate();
+			
+		} finally {
+			DBUtil.dbClose(con, ps, null);
+		}
+		
+		return result;
+	}
+
+
+	/**
+	 * Contest에 레코드 삭제
+	 * 공모전 정보 게시판의 게시글 삭제
+	 */
+	@Override
+	public int delete(String contestNum) throws SQLException {
+		Connection con = null;
+		PreparedStatement ps = null;
+		int result = 0;
+		
+		try {
+			
+			con = DBUtil.getConnection();
+			ps = con.prepareStatement("delete from contest where contestnum=?");
+			ps.setInt(1, Integer.parseInt(contestNum));
+			result = ps.executeUpdate();
+			
+		} finally {
+			DBUtil.dbClose(con, ps, null);
+		}
+		
+		return result;
 	}
 
 	
