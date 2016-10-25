3af522905460c23f2b2443e05a1c4bbb41607cad
joyoungyun <asacans@gmail.com>
2016-10-25 14:32:19 +0900

    20161025 회원정보수정

---
+++ src/kongs/controller/UpdateInfoAction.java
@@ -0,0 +1,27 @@
+package kongs.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class UpdateInfoAction implements Action {
+
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String url = "errorView/error.jsp";
+		
+		try {
+			//TODO 회원정보수정
+			//TODO 세션 리뉴얼
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+			request.setAttribute("errorMsg", e.getMessage());
+		}
+		
+		request.getRequestDispatcher(url).forward(request, response);
+	}
+
+}
